9ecd12e96bcef66ea290f14e86a189c8cdee6c8d
jing.zhu <jing.zhu@easystack.cn>
2020-01-10 14:56:29 +0800

    Add upgrade.html Add es403.html Change es404.html/es500.html

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ app.all('/', (req, res) => {
   var xCode = req.headers['x-code'];
   console.log("*********X-Code***************", xCode);
   if (xCode == 500){
-    res.status(500).sendFile(__dirname + '/content/500.html');
-  }else{
+    const page500 = fs.readFileSync(__dirname + '/content/500.html', 'utf8');
+    res.status(500).send(page500);  
+  } else if (xCode == 403){
+    const page403 = fs.readFileSync(__dirname + '/content/403.html', 'utf8');
+    res.status(403).send(page403); 
+  } else{
     const page404 = fs.readFileSync(__dirname + '/content/404.html', 'utf8');
     res.status(404).send(page404);
   }
@@ -19,6 +23,11 @@ app.all('/', (req, res) => {
 
 app.get('/healthz', (req, res) => {
   res.send('Healthy!').end();
+});
+
+app.get('/es-upgrade', (req, res) => {
+  const pageEsUpgrade = fs.readFileSync(__dirname + '/content/upgrade.html', 'utf8');
+  res.status(200).send(pageEsUpgrade);
 });
 
 app.listen(8080, () => console.log('App listening on port 8080'));
